d612837bd5e90497c64e5baba5c8fed668a011c7
harpreet kaur <harpreetvirdi17594@gmail.com>
2023-08-17 20:59:56 +0100

    updated index.html and code to fecth video

--- js/script.js
+++ js/script.js
@@ -7,23 +7,17 @@ var queryURL =
 https: showvideo.attr("style", "display:none;");
 showvideo2.attr("style", "display:none;");
 
-$("#clickme").on("click", function () {
-  fetch(queryURL)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-      showvideo.attr("style", "display:visible;");
-      showvideo2.attr("style", "display:visible;");
-      showvideo.attr(
-        "src",
-        "https://www.youtube.com/embed/" + data.items[0].id
-      );
-      showvideo2.attr(
-        "src",
-        "https://www.youtube.com/embed/" + data.items[0].id
-      );
-      // console.log(searchInputValue);
-    });
-});
+// $("#clickme").on("click", function () {
+fetch(queryURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    showvideo.attr("style", "display:visible;");
+    showvideo2.attr("style", "display:visible;");
+    showvideo.attr("src", "https://www.youtube.com/embed/" + data.items[0].id);
+    showvideo2.attr("src", "https://www.youtube.com/embed/" + data.items[0].id);
+    // console.log(searchInputValue);
+  });
+// });
