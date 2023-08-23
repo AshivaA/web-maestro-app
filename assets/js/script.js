3790a60481decbd257dda15c6c54262f3816fcd0
unknown <diloch111@gmail.com>
2023-08-24 00:47:29 +0100

    link submit button of learning styles to respective pages

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,9 @@
 var showvideo = $("#showvideo");
 var showvideo2 = $("#showvideo2");
-
 var queryURL =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=kUMe1FH4CHE&key=AIzaSyDlQmEuNWa7_szWM7C1tqYStAscXgQ4dL0";
-
-  
 https: showvideo.attr("style", "display:none;");
 showvideo2.attr("style", "display:none;");
-
 $("#clickme").on("click", function () {
   fetch(queryURL)
     .then(function (response) {
@@ -27,4 +23,29 @@ $("#clickme").on("click", function () {
       );
       // console.log(searchInputValue);
     });
+});
+// ===================================
+$("#save").on("click", function (event) {
+  event.preventDefault();
+  // if ($("#content1").attr("checked")) {
+  //   // event.preventDefault();
+  //   // $("#content2").toggle(this.checked);
+  //   window.location.href = "./assets/html/mypage.html";
+  // }
+  // window.location.href = "./assets/html/notes-page.html";
+  // if ($("#content1").is(":checked")) {
+  //   window.location.href = "./assets/html/mypage.html";
+  // }
+  // if ($("#content2").is(":checked")) {
+  //   window.location.href = "./assets/html/notes-page.html";
+  // }
+  if ($("#content2").is(":checked") && $("#content1").is(":checked")) {
+    alert("Choose one to proceed.");
+    return;
+  } else if ($("#content1").is(":checked")) {
+    window.location.href = "./assets/html/html-page.html";
+  }
+  if ($("#content2").is(":checked")) {
+    window.location.href = "./assets/html-docs/html-docs-page.html";
+  }
 });
